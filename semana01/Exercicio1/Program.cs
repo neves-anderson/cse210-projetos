7311cfef497fcd8751c5118e927541f5360301e8
neves-anderson <aneves1@byupathway.edu>
2026-09-03 15:16:22 -0400

    Exercicio1 finalizado

--- semana01/Exercicio1/Program.cs
+++ semana01/Exercicio1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio1.");
+        //Coleta do nome do usuário
+        Console.Write("Qual é o seu primeiro nome? ");
+        string primeiroNome = Console.ReadLine();
+
+        //Coleta do sobrenome do usuário   
+        Console.Write("Qual é o seu sobrenome? ");
+        string sobrenome = Console.ReadLine();
+        
+        //Exibe o mensagem com o nome do usuário
+        Console.WriteLine($"Seu nome é {sobrenome}, {primeiroNome} {sobrenome}.");
     }
 }
