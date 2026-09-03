b3c5f166e14480da8424e586cb2c255dde7d14ec
neves-anderson <aneves1@byupathway.edu>
2026-09-03 18:17:20 -0400

    Exercicio2 Finalizado

--- semana01/Exercicio2/Program.cs
+++ semana01/Exercicio2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Exercicio2.");
+        //Coleta do percentual da nota do usuário
+        Console.Write("Qual o percentual da sua nota? ");
+        string resposta = Console.ReadLine();
+        int percentual = int.Parse(resposta);
+
+        string grade = "";
+
+        if (percentual >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentual >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentual >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentual >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Sua nota é: {grade}");
+        
+        if (percentual >= 70)
+        {
+            Console.WriteLine("Você está aprovado!");
+        }
+        else
+        {
+            Console.WriteLine("Infelizmente, você não está aprovado. Mais sorte na próxima vez!");
+        }
     }
 }
