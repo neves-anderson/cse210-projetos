e2f7eaa24970f47e147756f4a1e3f436fec2d0d0
neves-anderson <aneves1@byupathway.edu>
2026-09-10 22:11:15 -0400

    Projeto

--- semana02/Curriculo/Program.cs
+++ semana02/Curriculo/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Curriculo.");
+        Emprego emprego1 = new Emprego();
+        emprego1._cargo = "Engenheiro de Software";
+        emprego1._empresa = "Microsoft";
+        emprego1._anoInicio = 2019;
+        emprego1._anoFim = 2022;
+
+        Emprego emprego2 = new Emprego();
+        emprego2._cargo = "Gerente";
+        emprego2._empresa = "Apple";
+        emprego2._anoInicio = 2022;
+        emprego2._anoFim = 2023;
+
+        Curriculo meuCurriculo = new Curriculo();
+        meuCurriculo._nome = "Carlos Barbosa";
+
+        meuCurriculo._empregos.Add(emprego1);
+        meuCurriculo._empregos.Add(emprego2);
+
+        meuCurriculo.Exibir();
     }
 }
